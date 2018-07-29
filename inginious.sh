c183d853c19b99a948ff03c7b774f33f16c69747
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-07-29 21:28:31 +0200

    Update inginious.sh

--- inginious.sh
+++ inginious.sh
@@ -1,1 +1,3 @@
 sudo curl -fsSL https://get.docker.com/ | sh #This will setup the Docker repo
+sudo usermod -aG docker ubuntu
+sudo apt-get install git mongodb gcc tidy python3 python3-pip python3-dev libzmq-dev
