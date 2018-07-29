fd245ce05e9f24b51829e3623973680875db3a60
Englert Ervin  Brunó <englert@users.noreply.github.com>
2018-07-29 21:46:22 +0200

    Update second.sh

--- second.sh
+++ second.sh
@@ -1,3 +1,9 @@
 sudo apt-get install -y git mongodb gcc tidy python3 python3-pip python3-dev libzmq-dev
+sudo apt-get install -y libldap2-dev libsasl2-dev libssl-dev
+sudo apt-get install -y libxmlsec1-dev libltdl-dev
+sudo systemctl start mongodb
+sudo systemctl enable mongodb
+sudo systemctl start docker
+sudo systemctl enable docker
 
 
